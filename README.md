1af385c3acf184927c0c77ae56be7db3b730b55e
Mathias Lux <mathias.lux@aau.at>
2015-09-03 08:20:42 +0200

    Added HelloWorld

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# esop14
+# ESOP Wintersemester 2015/16
 Demo and tutorial code for the programming course at Klagenfurt University
---
+++ src/at/aau/esop15/course00/HelloWorld.java
@@ -0,0 +1,10 @@
+package at.aau.esop15.course00;
+
+/**
+ * Created by mlux on 03.09.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
