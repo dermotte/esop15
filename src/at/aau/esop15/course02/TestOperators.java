a0b7d654695ecfafe4bdd6264fd2a1fb0f95d985
dermotte <keineres01>
2015-10-08 14:47:03 +0200

    type examples

--- src/at/aau/esop15/course02/TestOperators.java
+++ src/at/aau/esop15/course02/TestOperators.java
@@ -5,4 +5,17 @@ package at.aau.esop15.course02;
  * @author Dr. Mathias Lux, mlux@itec.aau.at, 08.10.2015.
  */
 public class TestOperators {
+    public static void main(String[] args) {
+
+        int i, j = 5;
+        short s = 4;
+        byte b;
+
+        i = j; 		// ok: derselbe Typ
+        i = 300; 	// ok (Zahlkonstanten sind int)
+        b = 122;	// falsch: 300 passt nicht in byte
+        i = s; 		// ok
+        // s = i; 		// falsch
+
+    }
 }
