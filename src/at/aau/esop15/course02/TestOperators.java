76ecc1f32686a5ddcf2dc3be84a666f2252c2ec2
dermotte <keineres01>
2015-10-08 13:21:02 +0200

    added empty class for todays lecture.

---
+++ src/at/aau/esop15/course02/TestOperators.java
@@ -0,0 +1,8 @@
+package at.aau.esop15.course02;
+
+/**
+ * A simple class for showing types and operators.
+ * @author Dr. Mathias Lux, mlux@itec.aau.at, 08.10.2015.
+ */
+public class TestOperators {
+}
