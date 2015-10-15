91e47a4d99e82cc8b74de1b7be0f8523dd9bce75
dermotte <keineres01>
2015-10-15 14:10:05 +0200

    1st try

--- src/at/aau/esop15/course03/IfExample.java
+++ src/at/aau/esop15/course03/IfExample.java
@@ -5,6 +5,16 @@ package at.aau.esop15.course03;
  */
 public class IfExample {
     public static void main(String[] args) {
-
+        // find maximum
+        int x = 12, y =42;
+        int maximum = -1;
+        if (x > y) {
+            maximum = x;
+        }
+        if (y >= x) {
+            maximum = y;
+        }
+        // output
+        System.out.println("maximum = " + maximum);
     }
 }
