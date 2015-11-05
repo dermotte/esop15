0288ab169612689bff258c5a759a2a287426e042
Mathias Lux <mathias.lux@aau.at>
2015-11-05 11:59:32 +0100

    removed some of the source to be implemented in the course.

--- src/at/aau/esop15/course05/MultiDimensionalArray.java
+++ src/at/aau/esop15/course05/MultiDimensionalArray.java
@@ -8,25 +8,9 @@ public class MultiDimensionalArray {
         int[][] myArray = new int[3][];
 
         // fill array with values to create a matrix
-        for (int i = 0; i < myArray.length; i++) {
-            myArray[i] = new int[] {i*3+1, i*3+2, i*3+3};
-        }
 
         // transpose matrix
-        for (int i = 0; i < myArray.length; i++) {
-            for (int j = 0; j < myArray[i].length; j++) {
-                if (j<i) {
-                    int tmp = myArray[i][j];
-                    myArray[i][j] = myArray[j][i];
-                    myArray[j][i] = tmp;
-                }
-            }
-        }
 
         // print array to System.out
-        for (int i = 0; i < myArray.length; i++) {
-            int[] ints = myArray[i];
-            System.out.printf("%2d\t%2d\t%2d\n", ints[0], ints[1], ints[2]);
-        }
     }
 }
